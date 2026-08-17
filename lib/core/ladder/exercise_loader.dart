800907e607249bedf1aff4b818eb97e0ebfbfe2e
Uli <uphone@gmx.de>
2026-08-17 20:18:41 +0200

    feat(ladder): ExerciseLoader builds rung-0 EncounterContent per refType

--- lib/core/ladder/exercise_loader.dart
+++ lib/core/ladder/exercise_loader.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
 
+import 'package:drift/drift.dart';
+
 import '../db/learning_db.dart';
 import '../script_profile.dart';
+import 'encounter.dart';
 import 'exercise_content.dart';
 import 'rung_defs.dart';
 
@@ -17,8 +20,7 @@ class ExerciseLoader {
     return switch (refType) {
       RefType.character => _loadCharacter(item.refId, exerciseType),
       RefType.lexeme => _loadLexeme(item.refId, exerciseType),
-      RefType.grammar =>
-        throw UnimplementedError('grammar exercises not yet supported'),
+      RefType.grammar => _loadGrammar(item.refId, exerciseType),
     };
   }
 
@@ -33,8 +35,15 @@ class ExerciseLoader {
     final reading = readings.isNotEmpty ? readings.first : '';
 
     return switch (type) {
-      ExerciseType.encounter =>
-        throw UnimplementedError('rung-0 encounter is built in a later task'),
+      ExerciseType.encounter => EncounterContent(
+          encounter: CharacterEncounter(
+            glyph: char.glyph,
+            reading: reading,
+            audioText: char.glyph,
+            strokeOrderAssetId: char.strokeOrderAssetId,
+            mnemonic: null,
+          ),
+        ),
       ExerciseType.recognition =>
         RecognitionContent(displayForm: char.glyph, answer: char.meaning),
       ExerciseType.readingInput =>
@@ -60,23 +69,59 @@ class ExerciseLoader {
           ..where((t) => t.id.equals(lexeme.conceptId)))
         .getSingle();
 
+    if (type == ExerciseType.encounter) {
+      final asset = await (_db.select(_db.assets)
+            ..where((t) =>
+                t.conceptId.equals(lexeme.conceptId) & t.type.equals('image')))
+          .getSingleOrNull();
+      return EncounterContent(
+        encounter: LexemeEncounter(
+          writtenForm: lexeme.writtenForm,
+          reading: lexeme.reading,
+          audioText: lexeme.writtenForm,
+          meaning: concept.glossKey,
+          conceptImagePath: asset?.path,
+          exampleSentence: null,
+        ),
+      );
+    }
+
     return switch (type) {
-      ExerciseType.encounter =>
-        throw UnimplementedError('rung-0 encounter is built in a later task'),
-      ExerciseType.recognition =>
-        RecognitionContent(displayForm: lexeme.writtenForm, answer: concept.glossKey),
+      ExerciseType.encounter => throw StateError('handled above'),
+      ExerciseType.recognition => RecognitionContent(
+          displayForm: lexeme.writtenForm, answer: concept.glossKey),
       ExerciseType.readingInput => ReadingInputContent(
-          displayForm: lexeme.writtenForm,
-          expectedReading: lexeme.reading,
-        ),
+          displayForm: lexeme.writtenForm, expectedReading: lexeme.reading),
       ExerciseType.productionInput => ProductionInputContent(
-          prompt: concept.glossKey,
-          expectedForm: lexeme.writtenForm,
-        ),
+          prompt: concept.glossKey, expectedForm: lexeme.writtenForm),
       ExerciseType.writeTrace => ProductionInputContent(
-          prompt: concept.glossKey,
-          expectedForm: lexeme.writtenForm,
-        ),
+          prompt: concept.glossKey, expectedForm: lexeme.writtenForm),
     };
+  }
+
+  Future<ExerciseContent> _loadGrammar(
+    String grammarId,
+    ExerciseType type,
+  ) async {
+    final gp = await (_db.select(_db.grammarPoints)
+          ..where((t) => t.id.equals(grammarId)))
+        .getSingle();
+    final canDo = await (_db.select(_db.canDoGoals)
+          ..where((t) => t.id.equals(gp.canDoId)))
+        .getSingle();
+
+    if (type == ExerciseType.encounter) {
+      return EncounterContent(
+        encounter: GrammarEncounter(
+          pattern: gp.id,
+          plainExplanation: canDo.description,
+          example: '',
+          canDoDescription: canDo.description,
+          contrast: null,
+        ),
+      );
+    }
+    // Graded grammar exercises are out of scope for this plan.
+    throw UnimplementedError('grammar graded exercises not yet supported');
   }
 }
